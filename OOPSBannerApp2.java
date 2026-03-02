96c0b10bd33929c597af7569f0b34f303f2f37be
Swarit-Srivastava <swarityashsrivastava@gmail.com>
2026-03-03 01:00:05 +0530

    Initial commit: add OOPSBannerApp2

---
+++ OOPSBannerApp2.java
@@ -0,0 +1,51 @@
+
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        String space = "   "; 
+        System.out.println(
+                "  *****  " + space +
+                "  *****  " + space +
+                " ********" + space +
+                " ********"
+        );
+        System.out.println(
+                " *     * " + space +
+                " *     * " + space +
+                " *     * " + space +
+                " *       "
+        );
+        System.out.println(
+                " *     * " + space +
+                " *     * " + space +
+                " *     * " + space +
+                " *       "
+        );
+        System.out.println(
+                " *     * " + space +
+                " *     * " + space +
+                " ********" + space +
+                " ********"
+        );
+        System.out.println(
+                " *     * " + space +
+                " *     * " + space +
+                " *       " + space +
+                "        *"
+        );
+        System.out.println(
+                " *     * " + space +
+                " *     * " + space +
+                " *       " + space +
+                "        *"
+        );
+        System.out.println(
+                "  *****  " + space +
+                "  *****  " + space +
+                " *       " + space +
+                " ********"
+        );
+    }
+}
